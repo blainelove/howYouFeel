ae5c165e93ce68dcf5b59324a167ed2ecd30f7cc
Kwaku <kwakugyapong219@gmail.com>
2021-03-10 12:16:14 -0500

    Patch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //test
 const imglist = document.querySelector("div#listsArr")
+const listForm = document.querySelector("form#listsArr")
 
 function listarrfun(){
 
@@ -24,9 +25,63 @@ function listarrfun(){
 }
 
 imglist.addEventListener("click", event => {
-    
-    if (event.target.tagName === "IMG"){
 
+    if (event.target.tagName === "IMG"){
+    
+        fetch(`http:localhost:3000/lists/${event.target.dataset.id}`)
+            .then(response => response.json())
+            .then(listObj => {
+            
+            const image = document.querySelector("img#image")
+            const span = document.querySelector("span#description")
+            const time = document.querySelector("span#time")
+        
+           
+           
+            listForm.dataset.id = listObj.id
+
+            image.src = listObj.image
+            span.textContent = listObj.description
+            time.textContent = listObj.created_at
+        
+
+
+        })
     }
 })
+
+
+listForm.addEventListener('submit', event => {
+event.preventDefault()
+
+    const image = event.target.image_pic.value
+    const description = event.target.description.value
+
+    const newObj = {image, description}
+
+    fetch(`http://localhost:3000/lists/${listForm.dataset.id}`,{
+         method: "PATCH",
+
+        headers: { "Content-Type": "application/json",
+                
+        },
+
+        body: JSON.stringify(newObj)
+
+})
+        .then(response => response.json())
+        .then(newObj => {
+
+        console.log(newObj)
+
+
+
+    })
+        
+})
+        
+        
+        
+
+
 listarrfun()
