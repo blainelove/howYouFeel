bd92a649974227593795ac6399e72fd4f7bd2328
Blaine Love <blainelove94@gmail.com>
2021-03-10 10:40:17 -0500

    fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 //test
-console.log(hello)
-console.log("Hello")
+
+function listarrfun(){
+
+    fetch("http:localhost:3000/lists")
+        .then(response => response.json())
+        .then(listarr => {
+            console.log(listarr)
+            listarr.forEach(list => {
+                const imgTag = document.createElement("img")
+                const pTag = document.createElement("p")
+                const imglist = document.querySelector("div#listsArr")
+
+                pTag.textContent = list.created_at
+                imgTag.src = list.image
+                
+                imglist.append(imgTag)
+                imglist.append(pTag)
+            })
+        })
+
+}
+listarrfun()
